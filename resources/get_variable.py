813d0e2ce89f87ef724b639bdf07f6295b5c463f
MaxKorbut <max.korbut0@gmail.com>
2021-04-30 18:28:11 +0300

    path 2

--- resources/get_variable.py
+++ resources/get_variable.py
@@ -2,18 +2,18 @@ import yaml
 
 
 def get_pet_variable(variable_name):
-    with open("/home/korbut/PycharmProjects/pet_store/resources/pet_data.yaml", "r") as f:
+    with open("./resources/pet_data.yaml", "r") as f:
         config = yaml.safe_load(f)
         return config[variable_name]
 
 
 def get_store_variable(variable_name):
-    with open("/home/korbut/PycharmProjects/pet_store/resources/store_data.yaml", "r") as f:
+    with open("./resources/store_data.yaml", "r") as f:
         config = yaml.safe_load(f)
         return config[variable_name]
 
 
 def get_user_variable(variable_name):
-    with open("/home/korbut/PycharmProjects/pet_store/resources/user_data.yaml", "r") as f:
+    with open("./resources/user_data.yaml", "r") as f:
         config = yaml.safe_load(f)
         return config[variable_name]
