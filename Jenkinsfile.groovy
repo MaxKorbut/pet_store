2c2d5d26efecdf532c41b628f6cbd18335de02a6
MaxKorbut <max.korbut0@gmail.com>
2021-04-30 18:47:17 +0300

    groovy 3

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -8,47 +8,42 @@ pipeline {
         label 'master'
     }
 
-    stage('Run Jenkins docker-slave'){
-        agent{
-            label "master"
-        }
-        stages{
-            stage('Run tests'){
-                steps{
-                    lock(env.email){
-                        script{
+    stages{
+        stage('Run tests'){
+            steps{
+                lock(env.email){
+                    script{
 
-                            def test_case = "./tests"
+                        def test_case = "./tests"
 
 
-                            try{
-                                def robotCmd = "robot"
-                                robotCmd = robotCmd + " " + "${test_case}"
-                                sh "${robotCmd}"
-                            }
-                            catch(Exception e){
-                                echo "Environment:" + e
-                                currentBuild.result = "UNSTABLE"
-                            }
+                        try{
+                            def robotCmd = "robot"
+                            robotCmd = robotCmd + " " + "${test_case}"
+                            sh "${robotCmd}"
+                        }
+                        catch(Exception e){
+                            echo "Environment:" + e
+                            currentBuild.result = "UNSTABLE"
                         }
                     }
                 }
             }
-            stage('Publish Robot Framework results'){
-                steps{
-                    step([
-                        $class : 'RobotPublisher',
-                        outputPath : '.',
-                        outputFileName : "output.xml",
-                        reportFileName : "report.html",
-                        logFileName : "log.html",
-                        disableArchiveOutput : false,
-                        enableCache: true,
-                        passThreshold : 100,
-                        unstableThreshold: 80.0,
-                        otherFiles : "*.png",
-                    ])
-                }
+        }
+        stage('Publish Robot Framework results'){
+            steps{
+                step([
+                    $class : 'RobotPublisher',
+                    outputPath : '.',
+                    outputFileName : "output.xml",
+                    reportFileName : "report.html",
+                    logFileName : "log.html",
+                    disableArchiveOutput : false,
+                    enableCache: true,
+                    passThreshold : 100,
+                    unstableThreshold: 80.0,
+                    otherFiles : "*.png",
+                ])
             }
         }
     }
